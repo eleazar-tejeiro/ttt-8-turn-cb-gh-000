616a4876fe4a71e23a5862cfda655447c0bfda29
Uriel Eleazar Tejeiro Garcia <eleazartejeiro@gmail.com>
2019-03-22 21:46:19 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ end
 # code your #valid_move? method here
 def valid_move?(tablero, indice)
   indice = indice.to_i
-  if position_taken?(tablero, indice) && indicie > 0
+  if position_taken?(tablero, indice) && indice > 0
     return true
   else
     return false
